1a160c665e52d77a03f9be2d200244f057ca9a0d
varidireddykirankumar <varidireddykirankumar@gmail.com>
2019-10-09 10:10:32 -0700

    last commit

--- fizzbuzz_kiran/fizzbuzz.py
+++ fizzbuzz_kiran/fizzbuzz.py
@@ -1,1 +1,12 @@
-this is initial
+
+for i in  range(100):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif i % 3 == 0:
+        print("fizz")
+        continue
+    elif i  % 5 == 0:
+        print("buzz")
+        continue
+    print(i) 
